d864c6715df45b6a1daf5da6f2ed8b08c8f386d4
git <claytnmorais@gmail.com>
2025-03-28 01:16:08 -0300

    Ajustando projeto bff--medical-consulting: logger dentro de app

--- app/logs/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-
-if not os.getenv("GEMINI_TOKEN"):
-    load_dotenv()
-
-OPENAI_TOKEN = os.getenv("GEMINI_TOKEN")
-GEMINI_AI_URL = os.getenv("GEMINI_AI_URL")
